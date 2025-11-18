a0383c97a6b8ff21c21d50fff3e205d51cb9149f
dorthyuser <74729522+dorthyuser@users.noreply.github.com>
2025-11-18 13:01:08 +0000

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,9 +9,14 @@ micronaut {runtime ("lambda_java");
               }
 }
 
-dependencies {implementation ("io.micronaut.platform:micronaut-platform:4.6.1");
-implementation (platform ("io.micronaut.aws:micronaut-aws-bom:4.6.1"));implementation ("io.micronaut.http-client");implementation ("io.micronaut.jackson");
-implementation ("jakarta.inject:jakarta.inject-api");testImplementation ("io.micronaut.test:micronaut-test-junit5")}
+dependencies {
+implementation ("io.micronaut.platform:micronaut-platform:4.6.1");
+implementation (platform ("io.micronaut.aws:micronaut-aws-bom:4.6.1"))
+implementation ("io.micronaut.http-client")
+implementation ("io.micronaut.jackson")
+implementation ("jakarta.inject:jakarta.inject-api")
+testImplementation ("io.micronaut.test:micronaut-test-junit5")
+}
 
 application {mainClass.set ("com.ai2dev.hellojava.Application")
 }
